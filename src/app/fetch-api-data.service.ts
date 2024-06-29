9a0702e56e65ac5c87bf32aa80021b2979556b5b
mikeb-molina <142452320+mikeb-molina@users.noreply.github.com>
2024-06-29 09:31:53 -0400

    Added user login form

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -21,7 +21,7 @@ export class UserRegistrationService {
   //Making the api call for the user registration endpoint
   public userRegistration(userDetails: any): Observable<any> {
     console.log(userDetails);
-    return this.http.post(apiUrl + 'users', userDetails).pipe(
+    return this.http.post(apiUrl + 'users/', userDetails).pipe(
     catchError(this.handleError)
     );
   }
@@ -29,7 +29,7 @@ export class UserRegistrationService {
   //Make the api call for the user login endpoint
   public userLogin(userDetails: any): Observable<any> {
     console.log(userDetails);
-    return this.http.post(apiUrl + 'users', userDetails).pipe(
+    return this.http.post(apiUrl + 'login/', userDetails).pipe(
       catchError(this.handleError)
     );
   }
@@ -37,7 +37,7 @@ export class UserRegistrationService {
   //Make the api call for the Get All Movies Endpoint
   getAllMovies(): Observable<any> {
     const token = localStorage.getItem('token');
-    return this.http.get(apiUrl + 'movies', {headers: new HttpHeaders(
+    return this.http.get(apiUrl + 'movies/', {headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
       })}).pipe(
@@ -85,7 +85,7 @@ export class UserRegistrationService {
   //Make the api call for the Get User Endpoint
   getUser( username: string): Observable<any> {
     const token= localStorage.getItem('token');
-      return this.http.get(apiUrl + 'users/' + username, {headers: new HttpHeaders(
+      return this.http.get(apiUrl + 'users' + username, {headers: new HttpHeaders(
         {
           Authorization: 'Bearer ' + token,
         })}).pipe(
---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { UserRegistrationService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrl: './user-login-form.component.scss'
+})
+export class UserLoginFormComponent implements OnInit {
+  @Input() userData= {Username: '', Password: ''};
+
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar) { }
+  
+ngOnInit(): void {}
+  
+  logInUser() : void{
+    this.fetchApiData.userLogin(this.userData).subscribe((result) => {
+      localStorage.setItem('user', JSON.stringify(result.user));
+      localStorage.setItem('token', result.token);
+      this.dialogRef.close();
+      this.snackBar.open(`Login Success, Welcome ${result.user.Username}`, 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      this.snackBar.open('Login Failed', 'OK', {
+        duration:2000
+      })
+    })
+  }
+}
+
